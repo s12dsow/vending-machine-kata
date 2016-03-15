daa81a863aabbe52a3b411e717dbafc32a41737b
Dicko Sow <S12.dsow@gmail.com>
2016-03-14 23:25:10 -0400

    refactored

--- python/tests/vending_machine_tests.py
+++ python/tests/vending_machine_tests.py
@@ -9,25 +9,33 @@ class TestVendingMachine(unittest.TestCase):
 
     def test_vending_machine_accepts_quarters(self):
         """should accept valid coins"""
-        self.assertEqual(self.vending_machine.accept_coins(25), self.vending_machine.current_amount)
+        self.vending_machine.accept_coins(25)
+
+        self.assertEqual(self.vending_machine.current_amount(), 25)
 
     def test_vending_machine_accepts_dimes(self):
         """should accept valid coins"""
-        self.assertEqual(self.vending_machine.accept_coins(10), self.vending_machine.current_amount)
+        self.vending_machine.accept_coins(10)
+
+        self.assertEqual(self.vending_machine.current_amount(), 10)
 
     def test_vending_machine_accepts_nickels(self):
         """should accept valid coins"""
-        self.assertEqual(self.vending_machine.accept_coins(5), self.vending_machine.current_amount)
+        self.vending_machine.accept_coins(5)
+
+        self.assertEqual(self.vending_machine.current_amount(), 5)
 
     def test_vending_machine_rejects_pennies_and_returns_them(self):
         """should reject and put pennies in coin return"""
-        self.assertEqual(self.vending_machine.accept_coins(1), self.vending_machine.coin_return)
+        self.vending_machine.accept_coins(1)
+
+        self.assertEqual(self.vending_machine.return_coins(), 1)
 
     def test_vending_machine_updates_display_when_valid_coin_accepted(self):
         """should update display when received valid coin"""
         self.vending_machine.accept_coins(25)
 
-        self.assertEqual(self.vending_machine.display(), "Current Amount: %d" % self.vending_machine.current_amount)
+        self.assertEqual(self.vending_machine.display(), "Current Amount: %d" % self.vending_machine.current_amount())
 
     def test_vending_machine_displays_insert_coin_when_no_coins_inserted(self):
         """should display 'INSERT COIN'"""
@@ -48,29 +56,31 @@ class TestVendingMachine(unittest.TestCase):
         self.assertEqual(self.vending_machine.display(), "THANK YOU")
 
         self.assertEqual(self.vending_machine.display(), "INSERT COINS")
-        self.assertEqual(self.vending_machine.current_amount, 0)
+        self.assertEqual(self.vending_machine.current_amount(), 0)
 
     def test_machine_displays_price_if_unsufficient_funds(self):
+        """should display price of item if not enough funds have been received"""
         self.vending_machine.select_product("cola")
 
         self.assertEqual(self.vending_machine.display(), "PRICE: 100")
-
         self.assertEqual(self.vending_machine.display(), "INSERT COINS")
 
     def test_product_is_dispensed_and_money_is_returned_if_money_left_over(self):
+        """should dispense products and give back any extra money"""
         self.vending_machine.accept_coins(25)
         self.vending_machine.accept_coins(25)
         self.vending_machine.accept_coins(25)
         self.vending_machine.accept_coins(25)
-        
+
         self.assertEqual(self.vending_machine.select_product("chips"), "chips")
-        self.assertEqual(self.vending_machine.coin_return, 50)
+        self.assertEqual(self.vending_machine.return_coins(), 50)
 
     def test_return_coins(self):
+        """should return all coins"""
         self.vending_machine.accept_coins(25)
-        self.vending_machine.return_coins()
+        self.vending_machine.refund_coins()
 
-        self.assertEqual(self.vending_machine.coin_return, 25)
+        self.assertEqual(self.vending_machine.return_coins(), 25)
         self.assertEqual(self.vending_machine.display(), "INSERT COINS")
 
 if __name__ == '__main__':
--- python/vending_machine/vending_machine.py
+++ python/vending_machine/vending_machine.py
@@ -1,6 +1,6 @@
 class VendingMachine:
     def __init__(self):
-        self.current_amount = 0
+        self.amount = 0
         self.coin_return = 0
         self.products = {"cola": 100, "chips": 50, "candy": 65}
         # self.products = {"cola": {'price': 100, 'quantity': 5}, "chips": 50, "candy": 65}
@@ -11,34 +11,40 @@ class VendingMachine:
 
         if coin_input == 1:
             self.coin_return += coin_input
-            return self.coin_return
 
         for coin in valid_coins:
             if coin_input == coin:
-                self.current_amount += coin_input
-                return self.current_amount
+                self.amount += coin_input
 
     def select_product(self, product):
-        if self.current_amount == self.products[product]:
-            self.current_amount -= self.products[product]
+        price = self.products[product]
+
+        if self.amount == price:
+            self.amount -= price
             self.display_message = "THANK YOU"
-            return product
-        elif self.current_amount > self.products[product]:
-            self.coin_return += self.current_amount - self.products[product]
-            return product
-        elif self.current_amount < self.products[product]:
-            self.display_message = "PRICE: %d" % self.products[product]
+        elif self.amount > price:
+            self.coin_return += self.amount - price
+        elif self.amount < price:
+            self.display_message = "PRICE: %d" % price
+            return
+        return product
+
+    def refund_coins(self):
+        self.coin_return += self.amount
+        self.amount = 0
 
     def return_coins(self):
-        self.coin_return += self.current_amount
-        self.current_amount = 0
+        return self.coin_return
+
+    def current_amount(self):
+        return self.amount
 
     def display(self):
         if self.display_message:
             msg, self.display_message = self.display_message, ""
             return msg
-        elif self.current_amount:
-            self.display_message = "Current Amount: %d" % self.current_amount
+        elif self.amount:
+            self.display_message = "Current Amount: %d" % self.amount
         else:
             self.display_message = "INSERT COINS"
 
